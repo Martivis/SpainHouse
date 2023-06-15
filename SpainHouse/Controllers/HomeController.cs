1d03b67bcf64a6ec7d450da0a5cdb26ef26405b5
Martivis <vanek-non@rambler.ru>
2023-06-15 19:41:54 +0300

    Main page

--- SpainHouse/Controllers/HomeController.cs
+++ SpainHouse/Controllers/HomeController.cs
@@ -15,7 +15,97 @@ namespace SpainHouse.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            return View(
+                new List<HouseViewModel>
+                {
+                    new HouseViewModel
+                    {
+                        Id = "R3304693",
+                        Town = "Fuengirola",
+                        Name = "Элитная вилла в стиле модерн",
+                        Price = 4195000,
+                        Area = 300,
+                        BedroomsCount = 5,
+                        Terrace = 25,
+                        ImageUrls = new List<string>
+                        {
+                            "https://ltdfoto.ru/images/2023/06/15/BBV-1.png"
+                        }
+                    },
+                    new HouseViewModel
+                    {
+                        Id = "R3304692",
+                        Town = "Fuengirola",
+                        Name = "Элитная вилла в стиле модерн",
+                        Price = 4183000,
+                        Area = 280,
+                        BedroomsCount = 6,
+                        Terrace = 23,
+                        ImageUrls = new List<string>
+                        {
+                            "https://ltdfoto.ru/images/2023/06/15/006_PEDROJAEN_DJI_0815-1.png",
+                            "https://ltdfoto.ru/images/2023/06/15/BBV-1.png"
+                        }
+                    },
+                    new HouseViewModel
+                    {
+                        Id = "R3304695",
+                        Town = "Fuengirola",
+                        Name = "Элитная вилла в стиле модерн",
+                        Price = 4195000,
+                        Area = 300,
+                        BedroomsCount = 5,
+                        Terrace = 25,
+                        ImageUrls = new List<string>
+                        {
+                            "https://ltdfoto.ru/images/2023/06/15/BBV-1.png",
+                            "https://ltdfoto.ru/images/2023/06/15/006_PEDROJAEN_DJI_0815-1.png"
+                        }
+                    },
+                    new HouseViewModel
+                    {
+                        Id = "R3304256",
+                        Town = "Fuengirola",
+                        Name = "Элитная вилла в стиле модерн",
+                        Price = 4183000,
+                        Area = 280,
+                        BedroomsCount = 6,
+                        Terrace = 23,
+                        ImageUrls = new List<string>
+                        {
+                            "https://ltdfoto.ru/images/2023/06/15/BBV-1.png",
+                            "https://ltdfoto.ru/images/2023/06/15/006_PEDROJAEN_DJI_0815-1.png"
+                        }
+                    },
+                    new HouseViewModel
+                    {
+                        Id = "R3304693",
+                        Town = "Fuengirola",
+                        Name = "Элитная вилла в стиле модерн",
+                        Price = 4195000,
+                        Area = 300,
+                        BedroomsCount = 5,
+                        Terrace = 25,
+                        ImageUrls = new List<string>
+                        {
+                            "https://ltdfoto.ru/images/2023/06/15/BBV-1.png"
+                        }
+                    },
+                    new HouseViewModel
+                    {
+                        Id = "R3304693",
+                        Town = "Fuengirola",
+                        Name = "Элитная вилла в стиле модерн",
+                        Price = 4195000,
+                        Area = 300,
+                        BedroomsCount = 5,
+                        Terrace = 25,
+                        ImageUrls = new List<string>
+                        {
+                            "https://ltdfoto.ru/images/2023/06/15/BBV-1.png"
+                        }
+                    },
+                });
         }
 
         public IActionResult Privacy()
---
+++ SpainHouse/Models/HouseViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace SpainHouse.Models
+{
+    public class HouseViewModel
+    {
+        public string Id { get; set; } = "";
+        public string Town { get; set; } = "";
+        public string Name { get; set; } = "";
+        public decimal Price { get; set; }
+        public int Area { get; set; }
+        public int BedroomsCount { get; set; }
+        public int Terrace { get; set; }
+        public List<string> ImageUrls { get; set; } = new List<string>();
+    }
+}
